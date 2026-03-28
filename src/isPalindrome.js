d0660ea30c56e69f2b95722e7fcb4532b76ea3e6
Inae Cho <inae.cho.mil@army.mil>
2026-03-28 18:21:17 -0500

    Completed 10 tests and wrote error-free code for isPalindrome function.

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,7 +1,34 @@
-function isPalindrome (input) {
-    if (input === "") return 0;
+function isPalindrome(input) {
 
-    return 1;
+    // If input is an empty string, return false.
+    if (typeof input !== "string") {
+        return false
+    }
+
+    if (input.trim() === "") {
+        return false
+    }
+
+    // Remove regex?
+    input = input.toLowerCase()
+
+    input = input.replace(/[^a-z]/g,"")
+
+    // Separate string into an array of individual characters.
+    let originalArray = input.split("")
+
+    // Reverse the array.
+    let reversedArray = originalArray.reverse()
+
+    // Turn reversed array back into string.
+    let reversedWord = reversedArray.join('')
+
+    // Compare word (original input with regex removed) with reversedWord.
+    return input === reversedWord
+
+
+
 }
 
 module.exports = isPalindrome;
+
--- tests/isPalindrome.test.js
+++ tests/isPalindrome.test.js
@@ -1,14 +1,65 @@
 const isPalindrome = require('../src/isPalindrome');
 
-/* Test 1 (Green): The test passes a function was written to return 0 for an empty string.
- */
-test('returns 0 for an empty string', () => {
-    expect(isPalindrome("")).toBe(0);
-});
+describe("isPalindrome", () => {
+    describe("Empty Strings", () => {
 
-/*
-Test 2 (Green): Single-word input. The function skips the if block because it is false; the test passes a literal string which means the input is NOT empty. For non-empty strings, the test will return 1. Therefore, the test passes.
- */
-test('return 1 for a single word', () => {
-    expect(isPalindrome("bob")).toBe(1)
+        test('returns false for an empty string', () => {
+            expect(isPalindrome("")).toBe(false);
+        })
+    })
+
+    describe("Single Word Input", () => {
+
+        test('returns true for simple palindrome', () => {
+            expect(isPalindrome("bob")).toBe(true)
+        })
+
+        test('returns false if input is not a palindrome', () => {
+            expect(isPalindrome("car")).toBe(false)
+        })
+
+    })
+
+    describe("Multiple Word Input", () => {
+
+        test('returns true for multi-word palindrome', () => {
+            expect(isPalindrome("lonely tylenol")).toBe(true)
+        })
+
+    })
+
+    describe("Case Sensitivity", () => {
+        test('returns true for capital letters in palindrome', () => {
+            expect(isPalindrome("lonely Tylenol")).toBe(true)
+        })
+    })
+
+    describe ("Whitespace Weaknesses", () => {
+        test('returns true for palindromes with leading spaces', () => {
+            expect(isPalindrome(" bob")).toBe(true)
+        })
+
+        test('returns true for palindromes with trailing spaces', () => {
+            expect(isPalindrome("bob ")).toBe(true)
+        })
+
+        test('return true for palindromes with multiple interior spaces as a separator', () => {
+            expect(isPalindrome("lonely   tylenol")).toBe(true)
+        })
+    })
+
+    describe("Punctuation", () => {
+        test('returns true for palindromes with punctuations', () => {
+            expect(isPalindrome("Madam, I'm Adam")).toBe(true)
+        })
+    })
+
+    describe('Non-String Inputs', () => {
+        test('handles non-string inputs gracefully', () => {
+            expect(isPalindrome(389)).toBe(false)
+            expect(isPalindrome(null)).toBe(false)
+            expect(isPalindrome(undefined)).toBe(false)
+            expect(isPalindrome({})).toBe(false)
+        })
+    })
 })
